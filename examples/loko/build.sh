47c1705cad02ac184d534911ee6e339a2701f0dd
Jason Eberle <jason@imgix.com>
2016-12-28 13:20:35 -0500

    Kill loko example files

--- examples/loko/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -xeo pipefail
-
-cd $SRCDIR && npm install && node_modules/.bin/bower install && node_modules/.bin/gulp build --env=prod
-mkdir -p $CANDIR/var/www
-cp -a $SRCDIR/.prod_srv/* $CANDIR/var/www
--- examples/loko/postactivate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -xeo pipefail
-
-circusctl restart imgix-app
