8d4ed3b63cf01d60ea914cf1840c0b45f2adc119
Jason Eberle <jason@imgix.com>
2017-08-29 19:18:36 -0400

    Remove all examples
    These are no longer accurate, so I'm removing them for now.

--- examples/gulpfile.js
+++
@@ -1,5 +0,0 @@
-var gulp = require('gulp'),
-    setupGulpTasks = require('web-tools/tools/gulp-tasks', gulp),
-    configFactory = require('./imgix.config.js');
-
-gulp = setupGulpTasks(gulp, configFactory);
