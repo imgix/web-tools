36c3d7c703b6f1a4c306e35ac7fcb4c2fe71648c
Jason Eberle <jason@imgix.com>
2017-12-29 21:53:56 -0500

    Fix utilities middleware

--- tools/middleware/middleware.utilities.js
+++ tools/middleware/middleware.utilities.js
@@ -2,12 +2,12 @@ var _ = require('lodash');
 
 module.exports = function configureServer(app, config) {
   // Add logging via Morgan middleware
-  if (_.get(config, 'options.logs')) {
+  if (_.get(config, 'server.options.logs')) {
     app.use(require('morgan')());
   }
 
   // Add gzipping via compression middleware
-  if (_.get(config, 'options.gzip')) {
+  if (_.get(config, 'server.options.gzip')) {
     app.use(require('compression')());
   }
 };
