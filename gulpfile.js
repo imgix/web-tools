f41b2410c3d9ba0fbe8961d2342df732ed4abfb9
Albert Mejia <mejia.albert@gmail.com>
2021-05-04 15:48:12 -0700

    Update local gulpfile with gulp.series()

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var _ = require('lodash'),
     gulp = require('gulp'),
     gutil = require('gulp-util'),
     args = require('yargs').argv,
-    runSequence = require('run-sequence'),
+    runSequence = require('gulp4-run-sequence'),
     jshint = require('gulp-jshint'),
     jscs = require('gulp-jscs'),
     jsonlint = require('gulp-json-lint'),
@@ -13,7 +13,54 @@ var _ = require('lodash'),
     gulpMetadata = require('./tools/gulp-metadata.js').applyTo(gulp);
 
 /*--- Check Tasks ---*/
-gulp.task('check-tools', function() {
+gulp.task('check-tools', gulp.series(checkTools), {
+  description: 'Run all tool files through JSHint and JSCS',
+  category: 'checker'
+});
+
+gulp.task('check-runcoms', gulp.series(checkRuncoms), {
+  description: 'Run all runcom files through JSONLint',
+  category: 'checker'
+});
+
+/*--- Default Task --*/
+gulp.task('default', gulp.series(defaultTask), {
+  description: 'Run the most important tasks for developing this project',
+  category: 'main',
+  weight: 0
+});
+
+/*--- Versioning Task ---*/
+gulp.task('version', gulp.series(version), {
+  description: 'Bump this project\'s semantic version number.',
+  category: 'misc',
+  arguments: {
+      'bump': '[Optional] Specifies which part of the version number (major, minor, patch) should be bumped.'
+    }
+});
+
+/*--- Help task ---*/
+gulp.task('help', gulp.series(help), {
+  description: 'List all available Gulp tasks and arguments.',
+  category: 'misc',
+  weight: 999
+});
+
+function checkRuncoms() {
+  return gulp.src(path.join('runcoms', '*.json'))
+    .pipe(jsonlint())
+    .pipe(jsonlint.report(jsonlintReporter));
+}
+
+function defaultTask(done) {
+  runSequence(
+    'check-tools',
+    'check-runcoms',
+    done
+  );
+}
+
+function checkTools() {
   return gulp.src(path.join('tools', '**', '*.js'))
     .pipe(jshint(_.merge(
         {lookup: false},
@@ -24,53 +71,17 @@ gulp.task('check-tools', function() {
         configPath: path.join(__dirname, '.', 'runcoms', 'rc.jscs.json')
       }))
     .pipe(jscs.reporter(jscsReporter.path));
-}, {
-  description: 'Run all tool files through JSHint and JSCS',
-  category: 'checker'
-});
+}
 
-gulp.task('check-runcoms', function() {
-  return gulp.src(path.join('runcoms', '*.json'))
-    .pipe(jsonlint())
-    .pipe(jsonlint.report(jsonlintReporter));
-}, {
-  description: 'Run all runcom files through JSONLint',
-  category: 'checker'
-});
-
-/*--- Default Task --*/
-gulp.task('default', function(done) {
-  runSequence(
-    'check-tools',
-    'check-runcoms',
-    done
-  );
-}, {
-  description: 'Run the most important tasks for developing this project',
-  category: 'main',
-  weight: 0
-});
-
-/*--- Versioning Task ---*/
-gulp.task('version', function() {
+function version() {
   var versionBump = require('./tools/misc/version-bump.js');
 
   return gulp.src(path.join('.', 'package.json'), {base: '.'})
     .pipe(versionBump(args.bump))
     .pipe(gulp.dest('.'));
-}, {
-  description: 'Bump this project\'s semantic version number.',
-  category: 'misc',
-  arguments: {
-      'bump': '[Optional] Specifies which part of the version number (major, minor, patch) should be bumped.'
-    }
-});
+}
 
-/*--- Help task ---*/
-gulp.task('help', function() {
-  gutil.log(gulpMetadata.describeAll());
-}, {
-  description: 'List all available Gulp tasks and arguments.',
-  category: 'misc',
-  weight: 999
-});
+function help(done) {
+  gutil.log(gulpMetadata.metadata.describeAll());
+  done();
+}
