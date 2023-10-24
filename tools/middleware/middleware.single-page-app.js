0baef249aa612e0e29c6ff40f2c8bfe9763740a2
Luis Ball <luqven@gmail.com>
2023-10-23 22:39:35 -0700

    feat: allow for spa proxy in development

--- tools/middleware/middleware.single-page-app.js
+++ tools/middleware/middleware.single-page-app.js
@@ -1,18 +1,20 @@
 var _ = require('lodash'),
-    express = require('express'),
-    STATIC_CACHE_AGE = 365 * 24 * 60 * 60 * 1000; // 1 year;
+  express = require('express'),
+  httpProxyMiddleware = require('http-proxy-middleware').createProxyMiddleware,
+  STATIC_CACHE_AGE = 365 * 24 * 60 * 60 * 1000; // 1 year;
 
 module.exports = function configureServer(app, config) {
   function serveIndex(request, response) {
     return response.sendFile('index.html', {
       root: config.appAssets.html.dest,
       headers: {
-          'Cache-Control': 'private',
-          'X-Frame-Options': 'DENY',
-          'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
-          'X-XSS-Protection': '1; mode=block',
-          'X-Content-Type-Options': 'nosniff'
-        }
+        'Cache-Control': 'private',
+        'X-Frame-Options': 'DENY',
+        'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Content-Type-Options': 'nosniff',
+        ...config.server?.options?.headers
+      }
     });
   }
 
@@ -25,6 +27,18 @@ module.exports = function configureServer(app, config) {
       next();
     }
   });
+
+  // Proxy routes
+  const reProxyRequest = config.server?.proxy?.matcher;
+  config.server?.proxy && app.use(
+    httpProxyMiddleware(
+      // Determine whether to forward this request or not
+      (pathname) => reProxyRequest.test(pathname),
+      {
+        target: config.server?.proxy?.target,
+      }
+    )
+  );
 
   // Serve static assets
   app.use(express.static(config.destPath, {
